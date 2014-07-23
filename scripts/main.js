234689569622b303fd849832d0a5116f94f7e304
aeleeds <adamleeds@adams-air-2.wp.comcast.net>
2014-07-23 17:33:59 -0400

    js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 function toggle_visibility(thechosenone) {
      $('.content').each(function(index) {
           if ($(this).attr("id") == thechosenone) {
-               $(this).show(200);
+               $(this).show();
           }
           else {
                $(this).hide();
