8ef5de4d96926fbcf6748685b0cbecd4b727fe41
aeleeds <a.e.leeds@gmail.com>
2016-02-25 02:18:02 -0500

    test version

---
+++ _posts/2016-02-24-first-post.markdown
@@ -0,0 +1,5 @@
+---
+title:  "\"Dreams in Cybernetic Fugue\" published"
+categories: new publication
+---
+My paper, "Dreams in Cybernetic Fugue" has been accepted for publication in *Historical Studies in the Natural Sciences* and will be forthcoming in the Spring 2016 issue.
---
+++ _site/scripts/main.js
@@ -0,0 +1,52 @@
+function toggle_visibilityOld(target) 
+{
+     $('.content').each(function(index) 
+     {
+          if ($(this).attr("id") == target) 
+          {
+               	$(this).show();
+          }
+          else 
+          {
+               $(this).hide();
+          }
+     });
+}
+
+function toggle_visibility(target) 
+{
+	/*var body = document.body,
+    html = document.documentElement;
+	var docHeight = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+	var spacer = document.getElementById('spacer');
+	console.log('Height is' + docHeight);
+	$(spacer).css('min-height', docHeight);*/
+	
+	console.log('Hide all but ' + target);
+	$('.content').each(function(index) 
+	{
+   		if ($(this).attr("id") != target)   
+   		{
+   			$(this).slideUp();
+   			console.log('Sliding up ' + $(this).attr('id'));
+   		}
+   	});
+   	
+   	setTimeout(function() 
+   	{
+		console.log('Slide down ' + target);
+		$('.content').each(function(index) 
+		 {
+			console.log('Checking ' + $(this).attr('id'));
+			if ($(this).attr("id") == target) 
+			{
+				$(this).slideDown();
+				console.log('Sliding down ' + $(this).attr('id'));
+			}
+		 });
+     }, 400);
+     
+     /*$(spacer).css('min-height','');*/
+}
+
